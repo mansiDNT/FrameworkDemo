8deb1f928edd2a2af7cf3703f2a78769905f4c6c
mansi <mansi.kiwiqa@gmail.com>
2021-07-04 15:57:23 +0530

    2

--- FrameworkDemo/src/main/java/com/index/LoginIndex.java
+++ FrameworkDemo/src/main/java/com/index/LoginIndex.java
@@ -16,7 +16,7 @@ public class LoginIndex extends SeleniumInit {
 	public void login() throws Exception
 	{
 		 System.out.println("Username is :" + Common.getCellValue("Sheet1",1,0));
-    	 System.out.println("Password is : " + Common.getCellValue("Sheet1",1,1));
+    	 System.out.println("Password is: " + Common.getCellValue("Sheet1",1,1));
    
     	 System.out.println(Common.getCellValue("Sheet1",1,0));
     	 System.out.println(Common.getCellValue("Sheet1",1,1));
