6f7dfaf8dd6e440e1085a946cea36e859473fc43
BAPS <mansi.d@dntinfotech.com>
2021-08-18 16:51:57 +0530

    new commit

--- src/main/java/com/index/LoginIndex.java
+++ src/main/java/com/index/LoginIndex.java
@@ -78,7 +78,7 @@ public class LoginIndex extends SeleniumInit {
 
 		testStepsLog("Step " + (stepCount++) + " : Verify 'Login' is successful.");
 		boolean errorMessage = loginVerificationPage.verifyNegativeLogin();
-		Assert.assertTrue(errorMessage, "User is not able to login successfully.");
+		Assert.assertTrue(errorMessage, "Login is not successful.");
 	}
 
 	@Test
