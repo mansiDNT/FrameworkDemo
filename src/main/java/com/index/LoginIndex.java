d1541c876b4218861c63360eb3b9da3c0773e013
BAPS <BAPS@DESKTOP-GKA213I>
2021-08-19 12:39:36 +0530

    test commit

--- src/main/java/com/index/LoginIndex.java
+++ src/main/java/com/index/LoginIndex.java
@@ -1,4 +1,7 @@
 package com.index;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
 import org.testng.annotations.Test;
@@ -7,70 +10,72 @@ import com.init.Common;
 import com.init.SeleniumInit;
 import com.relevantcodes.extentreports.LogStatus;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class LoginIndex extends SeleniumInit {
 	 
 		int stepCount = 1;
 		int extentStepCount =1;
-	@Test
-	public void login() throws Exception
-	{
-		 System.out.println("Username is:" + Common.getCellValue("Sheet1",1,0));
-    	 System.out.println("Password is : " + Common.getCellValue("Sheet1",1,1));
-   
-    	 System.out.println(Common.getCellValue("Sheet1",1,0));
-    	 System.out.println(Common.getCellValue("Sheet1",1,1));
 		
-		//login button disabled
-		testStepsLog("Step " + (stepCount++) + " : Enter username.");
-		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter username.");
-	//	loginVerificationPage = loginIndexPage.enterEmail((Common.getCellValue("Sheet1",1,0)).toString());
-		loginVerificationPage = loginIndexPage.enterEmail("qbankadmin");
-		test.log(LogStatus.PASS, "Username entered correctly.Entered username is : " +Common.getCellValue("Sheet1",1,0));
-		
-		testStepsLog("Step " + (stepCount++) + " : Enter password.");
-		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter password.");
-	//	loginVerificationPage = loginIndexPage.enterPassword((Common.getCellValue("Sheet1",1,0)).toString());
-	//	loginVerificationPage = loginIndexPage.enterEmail((Common.getCellValue("Sheet1",1,0)).toString());
-		loginVerificationPage = loginIndexPage.enterPassword("pass123");
-		test.log(LogStatus.PASS, "Password entered correctly.Entered username is : " +Common.getCellValue("Sheet1",1,0));
-		
-		testStepsLog("Step " + (stepCount++) + " : Click on Signin button.");
-		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Click on Sign In button.");
-		loginVerificationPage = loginIndexPage.clickOnLoginButton();
-		
-		testStepsLog("Step " + (stepCount++) + " : Verify 'Login' is successful.");
-		boolean errorMessage= loginVerificationPage.verifySuccessfulLogin();
-		Assert.assertTrue(errorMessage,"Login is not successful.");
-		
-	}
-	
 	@Test
 	public void login_negative() throws Exception
 	{
-		 System.out.println("Username is: " + Common.getCellValue("Sheet1",1,0));
-    	 System.out.println("Password is: " + Common.getCellValue("Sheet1",1,1));
-    	 
 		
-		//login button disabled
-		testStepsLog("Step " + (stepCount++) + " : Enter username.");
-		test1.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter username.");
-		//loginVerificationPage = loginIndexPage.enterEmail(Common.getCellValue("Sheet1",1,0));
-		loginVerificationPage = loginIndexPage.enterEmail("qbankadmin");
-		test1.log(LogStatus.PASS, "Username entered correctly.Entered username is : " +Common.getCellValue("Sheet1",1,0));
+		testStepsLog("Step " + (stepCount++) + " : Enter email address.");
+		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter username.");
+		loginVerificationPage = loginIndexPage.enterEmail(Common.getCellValue("Login",1,0));
+		test.log(LogStatus.PASS, "Username entered correctly.Entered username is : " +Common.getCellValue("Login",1,0));
+		Thread.sleep(3000);
 		
 		testStepsLog("Step " + (stepCount++) + " : Enter password.");
-		test1.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter password.");
-	//	loginVerificationPage = loginIndexPage.enterPassword(Common.getCellValue("Sheet1",1,0));
-		loginVerificationPage = loginIndexPage.enterPassword("pass123");
-		test1.log(LogStatus.PASS, "Password entered correctly.Entered username is : " +Common.getCellValue("Sheet1",1,0));
+		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter password.");
+		loginVerificationPage = loginIndexPage.enterPassword(Common.getCellValue("Login",1,1));
+		test.log(LogStatus.PASS, "Password entered correctly.Entered username is : " +Common.getCellValue("Login",1,1));
+		Thread.sleep(3000);
 		
-		testStepsLog("Step " + (stepCount++) + " : Click on Signin button.");
-		test1.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Click on Sign In button.");
+		testStepsLog("Step " + (stepCount++) + " : Click on Let's go button.");
+		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Click on Let's go button.");
 		loginVerificationPage = loginIndexPage.clickOnLoginButton();
+		Thread.sleep(3000);
+		
+		loginVerificationPage = loginIndexPage.ScrollDown();
+		Thread.sleep(3000);
+		
+		testStepsLog("Step " + (stepCount++) + " : Verify validation message is displayed or not.");
+		boolean validationmsg= loginVerificationPage.verifyNegativeLogin();
+		Assert.assertTrue(validationmsg,"validation messsage is displayed");
+		Thread.sleep(3000);
+	}
+	
+	@Test
+	public void login_positive() throws Exception
+	{
+		testStepsLog("Step " + (stepCount++) + " : Enter email address.");
+		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter username.");
+		loginVerificationPage = loginIndexPage.enterEmail(Common.getCellValue("Login",1,2));
+		test.log(LogStatus.PASS, "Username entered correctly.Entered username is : " +Common.getCellValue("Login",1,2));
+		Thread.sleep(3000);
+		
+		testStepsLog("Step " + (stepCount++) + " : Enter password.");
+		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Enter password.");
+		loginVerificationPage = loginIndexPage.enterPassword(Common.getCellValue("Login",1,3));
+		test.log(LogStatus.PASS, "Password entered correctly.Entered username is : " +Common.getCellValue("Login",1,3));
+		Thread.sleep(3000);
+		
+		testStepsLog("Step " + (stepCount++) + " : Click on Let's go button.");
+		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Click on Let's go button.");
+		loginVerificationPage = loginIndexPage.clickOnLoginButton();
+		Thread.sleep(3000);		
+		
+		testStepsLog("Step " + (stepCount++) + " : Click on three button.");
+		test.log(LogStatus.PASS, "Step " + (extentStepCount++) + " : Click on three button.");
+		loginVerificationPage = loginIndexPage.ClickonThreeDot();
+		Thread.sleep(3000);
 		
 		testStepsLog("Step " + (stepCount++) + " : Verify 'Login' is successful.");
-		boolean errorMessage= loginVerificationPage.verifyNegativeLogin();
-		Assert.assertTrue(errorMessage,"Login is not successful.");
+		boolean signoutText= loginVerificationPage.verifySuccessfulLogin();
+		Assert.assertTrue(signoutText,"Login is not successful.");
+		Thread.sleep(3000);
 	}
 }
